9cf87dbd2c8d49bfb868ba3d14fe4f301fc41931
Peyman Jahani <jahani.peyman@gmail.com>
2018-11-15 14:14:17 +0330

    Fix database long key error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        // Fix Error: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+        Schema::defaultStringLength(191);
     }
 
     /**
