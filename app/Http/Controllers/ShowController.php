e6ba7c74260c1a435f877a20e5256c52c05b2ce0
Peyman Jahani <jahani.peyman@gmail.com>
2018-11-18 18:26:01 +0330

    Define Model for ShowController

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -44,10 +44,10 @@ class ShowController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Show  $show
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Show $show)
     {
         //
     }
@@ -55,10 +55,10 @@ class ShowController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Show  $show
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Show $show)
     {
         //
     }
@@ -67,10 +67,10 @@ class ShowController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Show  $show
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Show $show)
     {
         //
     }
@@ -78,10 +78,10 @@ class ShowController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Show  $show
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Show $show)
     {
         //
     }
