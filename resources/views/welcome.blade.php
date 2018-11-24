68228f8b5f1a8f6b16c5d2c9674d0d804c690cd7
Peyman Jahani <jahani.peyman@gmail.com>
2018-11-24 05:36:23 +0330

    Improve welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,15 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
+                    <a href="{{ route('events.index') }}">Events</a>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
