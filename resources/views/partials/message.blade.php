7118210bf9499fb6e1d4c3e8c1d06396e29c757d
Peyman Jahani <jahani.peyman@gmail.com>
2018-12-05 10:34:37 +0330

    Small refactor session usage

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="alert {{ session()->get('alert-class', 'alert-info') }}" role="alert">
-                    {!! session()->get('message') !!}
+                <div class="alert {{ session('alert-class', 'alert-info') }}" role="alert">
+                    {!! session('message') !!}
                 </div>
             </div>
         </div>
