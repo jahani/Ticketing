29614813ef82f121ec83df11ccfff8a0999fc583
Peyman Jahani <jahani.peyman@gmail.com>
2018-12-11 03:37:39 +0330

    Move layout partial view to it's own directory

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
         </nav>
 
         <main class="py-4">
-            @include('partials.message')
+            @include('layouts.partials.message')
             @yield('content')
         </main>
     </div>
