c999b1e23aa8d7b672fe219c925f5d4597094a85
Peyman Jahani <jahani.peyman@gmail.com>
2018-12-15 01:44:05 +0330

    Show event data with persian jalali dates for shows

--- resources/js/components/shows/SingleShow.vue
+++ resources/js/components/shows/SingleShow.vue
@@ -4,6 +4,32 @@
     <div class="card-body">
       <h5 class="card-title">{{ event.name }}</h5>
       <h6>{{ event.status }}</h6>
+      <date-picker
+        v-model="show.start"
+        type="datetime"
+        format="YYYY-MM-DD HH:mm:ss"
+        disabled
+        display-format="jYYYY/jMM/jDD HH:mm, dddd"
+      >
+        <div slot="label">
+          <span>
+            <i class="fa fa-hourglass-start"></i>
+          </span>
+        </div>
+      </date-picker>
+      <date-picker
+        v-model="show.end"
+        type="datetime"
+        format="YYYY-MM-DD HH:mm:ss"
+        disabled
+        display-format="jYYYY/jMM/jDD HH:mm, dddd"
+      >
+        <div slot="label">
+          <span>
+            <i class="fa fa-hourglass-end"></i>
+          </span>
+        </div>
+      </date-picker>
       <small>{{ show.start }}</small>,
       <small>{{ show.end }}</small>
       <p class="card-text">{{ event.description }}</p>
@@ -13,6 +39,7 @@
 </template>
 
 <script>
+import VuePersianDatetimePicker from "vue-persian-datetime-picker";
 export default {
   props: {
     show: Object
@@ -25,7 +52,10 @@ export default {
   computed: {
     event: function() {
       return this.show.event;
-    },
+    }
+  },
+  components: {
+    datePicker: VuePersianDatetimePicker
   }
 };
 </script>
